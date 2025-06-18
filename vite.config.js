f1f3b395aaa42d9eac77812d1fe76a7a2f30bb0f
BelAnouar <anwarbelhassan@example.com>
2025-06-18 19:38:55 +0100

    Feat: Add Google Maps API loader, Formik, and Yup dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    
     svgr({
       svgrOptions: {
         icon: true,
@@ -14,6 +16,9 @@ export default defineConfig({
         namedExport: "ReactComponent",
       },
     }),
-  ],  base: "./",
+  ], resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }},  base: "./",
   server: { port: 3000 },
 });
